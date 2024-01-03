618d1d606bb6d0f9a18a2805ff995b703e897ed7
Nomika <nomigajula412@gmail.com>
2024-01-04 02:26:50 +0530

    content updated

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -6,17 +6,17 @@ function AboutCard() {
     <Card className="quote-card-view">
       <Card.Body>
         <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Nomika Gajula </span>
-            from <span className="purple"> Andhra Pradesh, India.</span>
-            <br></br>
-            <br /> I am a Final Year student pursuing B.Tech
-            in Computer Science and Engineering.
-            <br /><br></br>
-            Additionally, I am enthusiastic about Machine Learning and Data Science.
-            <br />
-            <br />
-            My pronouns are <i><b>Randomness Specialist / Professional Dabbler</b></i>
+          <p style={{ textAlign: "justify", fontSize:"90%" }}>
+          <ul>
+          {`\u2023`} Hi Everyone, I am <span className="purple">Nomika Gajula </span>
+            from <span className="purple" > Andhra Pradesh, India.</span>
+            <br></br><br></br>
+            {`\u2023`}  Final-year B.Tech student in Computer Science.
+            <br></br><br></br>
+            {`\u2023`} Passionate about Machine Learning and Data Science.
+            <br></br><br></br>
+            {`\u2023`} Tech enthusiast driving change through projects and hackathons, solving real-world problems and enhancing daily life.
+            </ul>
           </p>
         </blockquote>
       </Card.Body>
